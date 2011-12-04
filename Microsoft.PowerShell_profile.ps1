c662a502f79135751c5ca646a916b733e6dae076
Jason Jarrett <jason@elegantcode.com>
2011-12-04 09:57:02 -0800

    moved & renamed folders

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,20 @@
 
-foreach($file in (ls .\IncludeScripts\*.ps1))
+$globalProfileScriptsPath = (get-item .\GlobalScripts\)
+
+foreach($file in "$($globalProfileScriptsPath)\*.ps1")
 {
 	"Dot Sourcing Script - $file"
 	. $file
 }
 
+
+Remove-Item alias:cd
+Set-Alias cd (Get-Item "$($globalProfileScriptsPath)\Change-Directory.ps1")
+
+
 $tfPath = Find-Program 'Microsoft Visual Studio 10.0\Common7\IDE\TF.exe'
 function tf(){ & $tfPath $args }
+
 
 $notepadPath = Find-Program 'Notepad++\notepad++.exe'
 if($notepadPath)
