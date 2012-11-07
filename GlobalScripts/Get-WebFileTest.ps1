7b6a837b1aba9dc7428254648e3f2968d7d8f0f7
Jason Jarrett <jason@elegantcode.com>
2012-11-07 09:15:07 -0800

    added Get-WebFile script

---
+++ GlobalScripts/Get-WebFileTest.ps1
@@ -0,0 +1,29 @@
+﻿
+function Get-WebFileTest {
+    [CmdletBinding()]
+    param( 
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+        [string[]] $urls,
+        $fileName = $null,
+        [switch]$Passthru,
+        [switch]$quiet
+    )
+    BEGIN {
+        $webClient = New-Object System.Net.WebClient
+    }
+    PROCESS {
+
+        foreach($url in $urls) {
+
+            $file = $webClient.DownloadString($url)
+
+            New-Object Object `
+                | Add-Member NoteProperty Url $url -PassThru  `
+                | Add-Member NoteProperty Contents $file -PassThru
+        }
+    }
+    END {
+
+    }
+}
+set-alias wget Get-WebFileTest
