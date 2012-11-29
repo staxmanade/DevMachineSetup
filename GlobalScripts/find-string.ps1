c2c30ca292a760fda93593489c9f6023f1947908
Jason Jarrett <jason@elegantcode.com>
2012-11-29 14:49:36 -0800

    stole find-string from @PProvost

---
+++ GlobalScripts/find-string.ps1
@@ -0,0 +1,9 @@
+
+# stole this from Provost
+# https://github.com/PProvost/dotfiles/blob/master/powershell/find-string.ps1
+
+$local:command_usage = "usage: find-string glob text"
+if ($args.length -lt 2) { return ($command_usage) }
+get-childitem -recurse -include $args[0] | select-string $args[1] | group-object Path | select-object @{Expression={ $_.Name.Substring((get-location).Path.Length + 1) }; Name="Filename" }, @{Expression={ $_.Group | foreach-object { $_.LineNumber} }; Name="Line Numbers"} | format-table -Autosize
+
+
