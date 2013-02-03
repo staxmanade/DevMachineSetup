fb747d93705f5f213c2b70edbf63a5c66801d107
Jason Jarrett <jason@elegantcode.com>
2013-02-02 21:47:18 -0800

    added texteditor override for visual studio ctrl+w keyboard shortcut

--- GlobalScripts/Set-VisualStudioEnvironmentConfiguration.ps1
+++ GlobalScripts/Set-VisualStudioEnvironmentConfiguration.ps1
@@ -6,6 +6,7 @@ function Set-VisualStudioEnvironmentConfiguration(){
 
 	# Map Ctrl+W to close a tab
 	$DTE.Commands.Item("File.Close").Bindings = "Global::Ctrl+W";
+	$DTE.Commands.Item("File.Close").Bindings = "Text Editor::Ctrl+W"
 	
 	# Turn on line numbers for ALL language types
 	($DTE.Properties("TextEditor", "AllLanguages") | where {$_.Name -eq "ShowLineNumbers" } ).Value = $true
