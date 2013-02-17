40d0f7cf7a0e6283f09ceb024c02c7644c78c5c0
Jason Jarrett <jason@elegantcode.com>
2013-02-17 10:51:38 -0800

    created a gitlog helper function that prints the pretty graph to the console

--- GlobalScripts/gitHelpers.ps1
+++ GlobalScripts/gitHelpers.ps1
@@ -20,3 +20,11 @@ function gitGo($message) {
 
 
 }
+
+function gitlog() {
+
+	# found on StackOverflow
+	# http://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git
+
+	git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+}
