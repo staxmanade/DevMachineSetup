b802c2b1d314c607018591a448437dc6414f7e9c
Jason Jarrett <jason@elegantcode.com>
2013-08-21 14:58:32 -0700

    added a gitstandup command

--- GlobalScripts/gitHelpers.ps1
+++ GlobalScripts/gitHelpers.ps1
@@ -28,3 +28,8 @@ function gitlog() {
 
 	git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
 }
+
+function gitstandup() {
+
+	git log --author=Jason --graph --full-history --all --color --pretty=format:"%ar %x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+}
