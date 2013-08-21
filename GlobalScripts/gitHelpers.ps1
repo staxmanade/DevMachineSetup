302a47a4554240ad72acfc8b69087f15371d66e5
Jason Jarrett <jason@elegantcode.com>
2013-08-21 15:00:06 -0700

    added link to blog post

--- GlobalScripts/gitHelpers.ps1
+++ GlobalScripts/gitHelpers.ps1
@@ -31,5 +31,7 @@ function gitlog() {
 
 function gitstandup() {
 
+	# Good post on git pretty formatting
+	# http://alexefish.com/post/18453172089/pretty-git-logging
 	git log --author=Jason --graph --full-history --all --color --pretty=format:"%ar %x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
 }
